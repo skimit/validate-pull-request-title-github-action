0468e3e4391e7eb4a1781d73412e20b6ab12d86d
Rafael Almeida <rafaelcpalmeida@users.noreply.github.com>
2021-07-16 14:28:09 +0100

    GHA-00 Update validation RegEx

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,6 @@
 set -e
 
 # When updating this RegEx, please, keep in mind that Bash only supports POSIX. I.e.: Bash doesn't support \d, so you need to explicitly use [0-9] or [[:digit:]]
-echo "$1" | grep -oE '^([A-Z]*-[^-]*[[:digit:]])-?.*' -
+echo "$1" | grep -oE '^(fix|feature|breaking-change)(\/)?([A-Z]*-[^-]*[[:digit:]])-?.*' -
 
 [ $? -eq 0 ] && exit 0 || exit 1
